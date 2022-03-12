12bc03f378f60cf18337dbcf490e97d84a339f75
ΡΞΛΚSΤΞΡ <62203463+pkstDev@users.noreply.github.com>
2022-03-12 23:38:59 +0800

    Quick port

--- src/main/java/committee/nova/drinkit/mixin/MixinItemStack.java
+++ src/main/java/committee/nova/drinkit/mixin/MixinItemStack.java
@@ -3,7 +3,7 @@ package committee.nova.drinkit.mixin;
 import committee.nova.drinkit.DrinkIt;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.UseAction;
+import net.minecraft.util.UseAction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,8 +15,8 @@ public abstract class MixinItemStack {
     @Shadow
     public abstract Item getItem();
 
-    @Inject(method = "getUseAnimation", at = @At("RETURN"), cancellable = true)
-    public void onGetUseAnimation(CallbackInfoReturnable<UseAction> cir) {
+    @Inject(method = "getUseAction", at = @At("RETURN"), cancellable = true)
+    public void onGetUseAction(CallbackInfoReturnable<UseAction> cir) {
         final Item i = this.getItem();
         if (DrinkIt.DRINKABLE.contains(i)) cir.setReturnValue(UseAction.DRINK);
     }
